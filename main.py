f1c4fadfaae447ba84912bbecdbbede47bb25483
Yang Si Jun <aersijun@gmail.com>
2024-05-08 02:30:05 +0800

    using selenium for another website

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import re
+import time
 
 import numpy as np
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 main_df = pd.DataFrame()
 # month number mapping
@@ -14,42 +16,28 @@ month_dict = {"Mar": "03", "Apr": "04", "May": "05"}
 schedule_dict = {}
 print("Generating MPL results....")
 weeks_list = [1, 2, 3, 4, 5, 6, 7]
-# iterate through weeks
-for week in weeks_list:
-    URL = f"https://ph-mpl.com/schedule#week-{week}"
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html.parser")
-    schedule_frame = soup.find(id=f"week-{week}")
-    schedule_columns = schedule_frame.find_all("div", "col-lg-4")
-    # iterate through dates to get matches of that day
-    for column in schedule_columns:
-        # retrieve the date first
-        date = column.find("div", class_="header-subtitle").text.strip()
-        # convert this date to right format
-        day, month, year = date.split()
-        date_format = f"2024{month_dict[month]}{day}"
-        # retrieve the name of two teams from each card
-        teams = column.find_all("div", class_="team-name")
-        # Initialise empty list with date as key first
-        schedule_dict[date_format] = []
-        # Iterate through the matchups of the date
-        for i in range(0, len(teams), 2):
-            team1 = teams[i].text.strip().lower()
-            team2 = teams[i+1].text.strip().lower()
-            # add teams to a dict to iterate through later for match page
-            schedule_dict[date_format].append([team1, team2])
 
 # Iterate through all match dates and specific matches
-for date, matches in schedule_dict.items():
-    game_list = []
-    for match in matches:
-        URL = f"https://ph-mpl.com/data/match/{match[0]}-{match[1]}-{date}"
-        page = requests.get(URL)
-        soup = BeautifulSoup(page.content, "html.parser")
-        games = soup.find_all("div", id=lambda x: x and x.startswith('game'))
-        # iterate through the games
-        for game in games:
-            frames = game.findAll("td", attrs={"style":"max-width: 30px;"})
+URL = f"https://liquipedia.net/mobilelegends/MPL/Philippines/Season_13/Regular_Season#Week_1"
+from selenium import webdriver
+#set chromodriver.exe path
+driver = webdriver.Chrome()
+#implicit wait
+driver.get(URL)
+time.sleep(5)
+
+icons = driver.find_elements(By.CLASS_NAME, "brkts-match-info-icon")
+time.sleep(2)
+ad_popup = driver.find_element(By.ID, "top-ad")
+ad_popup.click()
+for icon in icons:
+    icon.click()
+    info_frame = driver.find_element(By.CLASS_NAME, "brkts-popup")
+    print(info_frame)
+    # get rid of the popup
+    icon.click()
+# To access the stats we need to click one by one then put in our df
+# iterate through the games
             # heroes = game.findAll(string=re.compile("[A-Za-z]"))
             # print(heroes)
 
